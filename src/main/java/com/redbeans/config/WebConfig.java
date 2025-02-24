d7dd107e3e992a0ece77a466728485603081b52e
[Namung2] <[mungf0033@gmail.com]>
2025-02-25 02:34:55 +0900

    Fix CORS configuration for credentials

--- src/main/java/com/redbeans/config/WebConfig.java
+++ src/main/java/com/redbeans/config/WebConfig.java
@@ -10,7 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // React 개발 서버 PORT 번호
+                .allowedOrigins("http://localhost:3000",// React 개발 서버 PORT 번호 3000(내꺼)
+                        "https://warm-blini-39cdcb.netlify.app" // Netlify 배포 URL
+                        )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
